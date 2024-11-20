358455b69ff4a4d194da05e4d1949108b25e57fa
Vishnu K V <112655184+Vishnukv07@users.noreply.github.com>
2024-11-20 04:35:07 +0000

    new changes

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ class Contact(db.Model):
 
 
 def get_projects():
-    api_url = f'https://api.github.com/users/dmdhrumilmistry/repos'
+    api_url = f'https://api.https://github.com/Vishnukv07/Portfolio'
     cards_list = requests.get(api_url).json()
     return cards_list
 
